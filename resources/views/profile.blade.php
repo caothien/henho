7fbda774067840f5858c9b6a7008707ce5345f85
Thien Cao <caohoangthien94@gmail.com>
2016-12-28 23:54:53 +0700

    fix like facebook

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -97,9 +97,9 @@
 
 					<hr>
 
-					<div class="fb-like" data-href="http://localhost:8000/profile/{{$profile->created_at}}/{{$profile->id}}" data-width="400" data-layout="standard" data-action="like" data-size="large" data-show-faces="true" data-share="true"></div>
+					<div class="fb-like" data-href="http://henhosinhvien.com/profile/{{$profile->created_at}}/{{$profile->id}}" data-width="400" data-layout="standard" data-action="like" data-size="large" data-show-faces="true" data-share="true"></div>
 
-					<div class="fb-comments" data-href="http://localhost:8000/profile/{{$profile->created_at}}/{{$profile->id}}" data-width="100%" data-numposts="10"></div>
+					<div class="fb-comments" data-href="http://henhosinhvien/profile/{{$profile->created_at}}/{{$profile->id}}" data-width="100%" data-numposts="10"></div>
 			</div>
 		</div>
 	</div>
